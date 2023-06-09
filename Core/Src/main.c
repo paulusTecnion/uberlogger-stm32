2826f76824d34146c73bfafa92f95d9a7861fed0
Paulus Potter <paulus@tecnion.nl>
2023-06-09 23:26:10 +0200

    Improved single shot performance

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -656,7 +656,7 @@ int main(void)
 		  {
 			  htim3_bak = htim3;
 			  // Set timer to 100Hz
-			  Config_Set_Sample_freq(ADC_SAMPLE_RATE_100Hz);
+//			  Config_Set_Sample_freq(ADC_SAMPLE_RATE_100Hz);
 
 			  tim3_counter = 0;
 			  adc_is_half = 0;
@@ -698,7 +698,7 @@ int main(void)
 				  spi_ctrl_receive(cmd_buffer, sizeof(spi_cmd_t));
 			  }
 			  // limit our acquisition to 3 samples
-			  if (gpio_result_write_ptr > 3)
+			  if (gpio_result_write_ptr > 2)
 			  {
 				  // uint16_t *adcData = (uint16_t*)(spi_msg_1_ptr->adcData);
 
