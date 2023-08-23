da64e55fe1094fb5fb2ffbebb9a282a4a48b116e
Paulus Potter <paulus@tecnion.nl>
2023-08-23 20:23:32 +0200

    iir filter comment cleanup

--- Core/Src/iirfilter.c
+++ Core/Src/iirfilter.c
@@ -3,22 +3,13 @@
 #include "iirfilter.h"
 //#include "settings.h"
 
-// Original coefficients from https://tecnionnl.sharepoint.com/:x:/s/uberlogger/EeEoN_zLy7BHslnFgKYobd4BH9o46vYH16z9PU2SE_CJCw?e=e9fFJc
-// 0.085849253
-// 0.164328412
-// 0.324768093
-// 0.544061872
-// 0.792120424
-// 0.956786082
-//#define TAG_IIR "IIR"
+
 #define FIXEDPT_WBITS 16
 #include "fixedptc.h"
 
 
 #define NUM_COEFFICIENTS 8   // Filter length
 #define NUM_ADC_CHANNELS 8
-
-//const int64_t c[NUM_COEFFICIENTS] = {8584925, 16432841, 32476809, 54406187, 79212042, 95678608, 95678608, 95678608};      // mulitplied with 100000000
 
  fixedpt cfl[NUM_COEFFICIENTS];
 
@@ -33,32 +24,19 @@ uint16_t round_value(uint32_t val) {
     return (val + 32768) >> 16;  // Add half of 2^16 to round, then shift
 }
 
-uint16_t divRoundClosest(const uint32_t n)
-{
-  const uint16_t d = 32768;
-  return ((n < 0) == (d < 0)) ? ((n + d/2)/d) : ((n - d/2)/d);
-}
+
 
 uint32_t temp[NUM_ADC_CHANNELS] = {0};
 
 void iir_filter(uint16_t * input, uint16_t * output, uint8_t channel)
 {
-    // Based on the factor, we need to pick the correct coefficients 
 
-    // Multiply and accumulate
-    // ESP_LOGI(TAG_IIR, "input: %ld, coeff: %lld, y_state: %lld", input, c[channel][coeff_index], y_state[channel]);
-//    y_state[channel] = ((c[coeff_index] * (int64_t)*input ) + ((100000000LL-c[coeff_index]) * y_state[channel]))/ 100000000LL;
-//     input_fp = fixedpt_fromint(*input);
-//     output_fp = fixedpt_fromint(y_state[channel]);
 	temp[channel] = temp[channel] + cfp * (*input - *output);
-//	y_state[channel] = divRoundClosest(temp[channel]);
+
 	y_state[channel] = round_value(temp[channel]);
 
 	*output = y_state[channel];
 
-//     y_state[channel] += fixedpt_toint(fixedpt_mul(cfp, input_fp-output_fp));
-    // the factor 1000000 is used 
-//    *output = (uint16_t)(y_state[channel]);
 }
 
 void iir_reset()
@@ -90,6 +68,7 @@ uint8_t iir_set_samplefreq(uint8_t sampleFreq)
 
 void iir_init()
 {
+	// Original coefficients from https://tecnionnl.sharepoint.com/:x:/s/uberlogger/EeEoN_zLy7BHslnFgKYobd4BH9o46vYH16z9PU2SE_CJCw?e=e9fFJc
 	//{ 0.00184806, 0.003692705, 0.00920621, 0.018327665, 0.045191272, 0.088340294, 0.16887658, 0.370256345};
 	cfl[0] = fixedpt_rconst(0.00184806);
 	cfl[1] = fixedpt_rconst(0.003692705);
